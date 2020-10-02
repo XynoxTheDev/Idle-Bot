e65d54dc3c38495993bd351c42c3bb3f127d180d
Arindamz <arindamhazra987@gmail.com>
2020-10-02 18:49:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// require the discord.js module
+const Discord = require('discord.js');
+
+// create a new Discord client
+const client = new Discord.Client();
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+    console.log('Ready!');
+
+    // Set the client user's activity
+    // Set the client user's status
+    client.user.setPresence({ game: { name: `dp!help | Xynox The Dev` }, status: 'idle' });
+
+});
+
+client.on('message', message => {
+
+    if (message.content === 'dp!help') {
+        message.channel.send('The developers are working in the backend.');
+    }
+
+});
+
+// login to Discord with your app's token
+client.login('NzU5NzYzODU1NjgwNjAyMTIy.X3CPOg.9sCNeizmRloM9hRDtoCGbdPebqY');
