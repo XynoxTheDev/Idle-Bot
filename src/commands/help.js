60f0c9376d9ee70fd9e714f24a5548d97966ee4f
Arindamz <workforxynox@gmail.com>
2020-10-06 17:36:48 +0530

    Update help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,18 +1,17 @@
 const { Command } = require('discord-akairo');
 
-class help extends Command {
+class Help extends Command {
     constructor() {
         super('help', {
-            aliases: ['halp'],
-            clientPermissions: ['SEND_MESSAGES'],
-            userPermissions: ['SEND_MESSAGES']
-
+            aliases: ['help']
         });
     }
 
     exec(message) {
+
         message.reply('The developers are working in the backend for the current issue/maintenance. Stay tuned to the <https://discord.gg/ZzbZpdw> for updates related to the bot.')
+
     }
 }
 
-module.exports = help;
+module.exports = Help;
